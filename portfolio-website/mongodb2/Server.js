2d6bd2c6212e2a89adcecf0ca88742700398f72e
Devang Singh Parihar <parihardevangsingh11@gmail.com>
2025-01-11 18:57:04 +0530

    commit admit

---
+++ portfolio-website/mongodb2/Server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const port = 3000;
+const app = express();
+const Contact = require('./Contact.jsx');
+
+
+
+// // Connect to MongoDB
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './Contact.jsx'));
+}
+);
+
+app.listen(port, () => {
+  console.log(`Server is running`);
+});
