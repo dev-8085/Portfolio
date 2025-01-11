f9a3edec33b562433ddffca4ff4db0c50facb3eb
Devang Singh Parihar <parihardevangsingh11@gmail.com>
2025-01-11 13:02:21 +0530

    added commit

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,13 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Projects from "./Components/Project";
 import Contact from "./Components/Contact";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/data')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <Hero />
+      <Hero data={data} />
       <Projects />
       <Contact />
     </div>
