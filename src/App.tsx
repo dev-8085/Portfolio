9a03ee779c85e827c3c7b11114755d2657e15605
Devang Singh Parihar <parihardevangsingh11@gmail.com>
2025-01-22 13:58:12 +0530

    added commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import { lazy, Suspense } from "react";
+import React, { useRef, Suspense } from "react";
 import "./App.css";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const CharacterModel = lazy(() => import("./components/Character"));
 const MainContainer = lazy(() => import("./components/MainContainer"));
 import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
+  const container = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(".circle", { x: 360 });
+  }, { scope: container });
+
   return (
-    <>
-      <LoadingProvider>
-        <Suspense>
-          <MainContainer>
-            <Suspense>
-              <CharacterModel />
-            </Suspense>
-          </MainContainer>
-        </Suspense>
-      </LoadingProvider>
-    </>
+    <LoadingProvider>
+      <Suspense fallback={<div>Loading MainContainer...</div>}>
+        <MainContainer ref={container}>
+          <Suspense fallback={<div>Loading Character...</div>}>
+            <CharacterModel />
+          </Suspense>
+        </MainContainer>
+      </Suspense>
+    </LoadingProvider>
   );
 };
 
